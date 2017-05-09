0d986396c7f9654d3cb8ba71c1a19dd303412c33
ipaulo <1xolodec@gmail.com>
2017-05-09 13:57:34 +0300

    Addes functinos for gradient compuation and setting of new parameters

--- ga3c/Database.py
+++ ga3c/Database.py
@@ -6,20 +6,21 @@ import logging
 from io import BytesIO
 
 
-class Database(object):
+class DatabaseInterface(object):
     """Abstract class, TBD"""
     pass
 
 
-class RedisDB(Database):
+class RedisInterface(DatabaseInterface):
 
     def __init__(self, host, port):
         self.host = host  # TODO: do we need to store host and port?
         self.port = port
         self.prefix = "{}.{}.".format(socket.gethostname(),  os.getpid())
-
+        self.params_prefix = "network_params"  # this should be same for all instances
+        self.gradients_prefix = "gradients"  # this should be same for all instances
+        self.connection = redis.Redis(host=self.host, port=self.port)
         try:
-            self.connection = redis.Redis(host=self.host, port=self.port)
             self.connection.client_list()
         except redis.ConnectionError:
             logging.FATAL("Redis on {}:{} could not be reached!".format(self.host, self.port))
@@ -35,17 +36,17 @@ class RedisDB(Database):
     def _load_obj_from_str(string):
         return joblib.load(BytesIO(string))
 
+    def append_gradients(self, grads):
+        self.connection.rpush(self.gradients_prefix, self._dump_obj_to_str(grads))
 
-
-    def save_session(self, observations, actions, rewards, is_alive ):
-
-
+    def save_session(self, observations, actions, rewards, memories=None):
+        pass
 
     def save_params(self, params):
         pass
 
-    def load_params(self, params):
-        pass
+    def load_params(self):
+        return self._load_obj_from_str(self.connection.get(self.params_prefix))
 
 
 
--- ga3c/GA3C.py
+++ ga3c/GA3C.py
@@ -53,6 +53,11 @@ if Config.PLAY_MODE:
     Config.TRAIN_MODELS = False
     Config.SAVE_MODELS = False
 
+
+assert Config.DUAL_RMSPROP is False, "We have broken this option"
+assert Config.TENSORBOARD is False, "We have broken this option"
+
+
 gym.undo_logger_setup()
 
 # Start main program
